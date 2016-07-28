64ba9f80409ed9326f6d115d6600828b868816ec
Tomo Masakura <fj0706kr@aa.jp.fujitsu.com>
2016-07-28 16:45:55 +0900

    Refactoring

--- scripts/fs-util.js
+++ scripts/fs-util.js
@@ -21,37 +21,6 @@ var fsUtil = {
   copy: function (src, dest) {
     return this.execFile('cp', [src, dest]);
   },
-  copyFiles: function (srcDir, destDir, filesOrFilter) {
-    var that = this;
-
-    var enumerateFiles;
-    if (typeof filesOrFilter === 'function') {
-      enumerateFiles = function () {
-        return that.readdir(srcDir)
-          .then(function (files) {
-            return files.filter(filesOrFilter);
-          });
-      };
-    } else {
-      enumerateFiles = function () {
-        return Promise.resolve(filesOrFilter);
-      };
-    }
-
-    return enumerateFiles()
-      .then(function (files) {
-        var copies = files.map(function (file) {
-          var src = path.join(srcDir, file);
-          var dest = path.join(destDir, file);
-
-          return that.copy(src, dest)
-            .then(function () {
-              console.log('Copy to ' + dest);
-            });
-        });
-        return Promise.all(copies);
-      });
-  },
   readdir: function (dir, options) {
     return new Promise(function (resolve, reject) {
       fs.readdir(dir, options, function (error, files) {
